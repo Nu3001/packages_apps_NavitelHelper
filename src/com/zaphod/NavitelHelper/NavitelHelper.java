148a6cf387bfcfa268a814a71bc41240d0243a2b
Mike Wilson <mwilson@acesappraisal.com>
2015-06-14 08:44:14 -0400

    initial commit
    Signed-off-by: Mike Wilson <mwilson@acesappraisal.com>

---
+++ src/com/zaphod/NavitelHelper/NavitelHelper.java
@@ -0,0 +1,43 @@
+package com.zaphod.NavitelHelper;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.lang.Override;
+
+public class NavitelHelper extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        PackageManager pm = getPackageManager();
+        Intent launch = pm.getLaunchIntentForPackage("com.navitel");
+        if (launch != null) {
+            try {
+                startActivity(launch);
+            } catch (ActivityNotFoundException anf) {
+                finish();
+            }
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    
+}
